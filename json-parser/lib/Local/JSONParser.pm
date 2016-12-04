935d577e5f348d8c2012664da8bfd75111e29b9c
surk0ff <writeln7@gmail.com>
2016-12-04 21:18:08 +0300

    JSON PARSER

--- json-parser/lib/Local/JSONParser.pm
+++ json-parser/lib/Local/JSONParser.pm
@@ -1,52 +1,88 @@
-package Local::JSONParser;
-use JSON::XS;
+#
+#===============================================================================
+#
+#         FILE: JSONParser.pm
+#
+#  DESCRIPTION:
+#
+#        FILES: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 03.12.2016 20:50:16
+#     REVISION: ---
+#===============================================================================
+
+package Local::JSONParser v1.0.0;
 
 use strict;
 use warnings;
+use utf8;
+
+use DDP;
 use base qw(Exporter);
 our @EXPORT_OK = qw( parse_json );
 our @EXPORT = qw( parse_json );
-use DDP;
-use feature 'say';
-use utf8;
+
+sub clear_string{
+	my $string = shift;
+my $substr;
+	if ($string =~ /\\n/gc) {}
+	if ($string =~ /\\/gc) {}
+
+
+	while(!(/\G\"/gc)) {
+		  if (/\G\\n/gc) {$substr = $substr . chr(10);}
+		                  elsif (/\G\\\"/gc) {$substr = $substr . chr(34);}
+		                  elsif (/\G\\u(\w{3,4})/gc) {$substr = $substr . chr(hex($1));}
+		                  elsif (/\G\\t/gc) {$substr = $substr . chr(9);}
+		                 elsif (/\G\\/gc) {die "Invalid JSON sequence!";}
+		                 else {
+		                     /\G([^"\\]*)/gc;
+		                     $substr = $substr . $1;
+		                 }
+		             }
+		             return $substr;
+
+			  }
 
 sub parse_json {
 	my $source = shift;
-my %hash;
-#return JSON::XS->new->utf8->decode($source);
-for ($source) {
-	while (pos() < length()) {
-				if    (/\G[\,\t\n\s]?"([^\{\[\(\]\}\)]+?)":\s?(\{.+?\})+[\,\t\n\s]*?(?=\")?/gc) {				#разобраться с положением ифов, до этого это стояло в конце
-			my $str = $2;
-			my $key = $1;
-			say "$key     and     $str";
-			$hash{$key} =  parse_json($str);
-        }
-		elsif (/\G[,\t\n]*?"([^\{\[\(\]\}\)]+?)":\s?"([\w\\\s]*?)"[,\t\n\s]*/gc) {
-			my $str = $2;
-			my $key = $1;
-			$str =~ s/\\u(.{3,4})/my $num = hex($1); chr($num)/ge;
-			$str =~ s/(\\n)/chr(10)/ge;
-			$str =~ s/(\\t)/chr(9)/ge;
-			$str =~ s/(\\")/chr(34)/ge;
-			$hash{$key} = $str;
-		}
-		elsif (/\G[,\t\n]*?"([^\{\[\(\]\}\)]+?)":\s?(-?\d+?[\.eE]?\d*[-+]*\d*)[,\t\n\s]*/gc) {
-			$hash{$1} = 0+$2;
-		}
-		elsif (/\G[,\t\n]*?"([^\{\[\(\]\}\)]+)?":\s?\[(.+?)\][,\t\n\s]*/gcs) {
-			my @t = split (/(?<!\\"),[\s]*/g,$2);		#my @t = split (/(?<!\\[\"]),[\s]*/g,$2);   ой ли
-			my $i = 0;
-			for my $item (@t) {				#$item =~ s/\\u(.{3,4})/my $num = hex($1); chr($num)/ge;
-				if (/\D+/) {				#$item =~ s/(\\n)/chr(10)/ge;
-				$t[$i++] =~ s/\"//gc;			#$item =~ s/(\\t)/chr(9)/ge;
-				}					#$item =~ s/(\\")/chr(34)/ge;
-			}
-			$hash{$1} = (\@t);
-		}
-	else { pos()++; };
+	
+	my %hash;
+	my $first = substr $source, 0, 1;
+	if ($first eq '[') {
+			$source =~ /\[(.*?)\]/gc;
+			my $get_array = $1;
+			my @arr = split(/,/, $get_array);
+			return \@arr;	
 	}
+	else {		for ($source){
+				my $line = $_;
+					while ($line =~ /"(\w+)":/g) { 	
+						my $key = $1;
+						$line =~ /\w/g;
+						if 	($line =~ /$key":\s"(.+?)"/) {
+			   				my $substr = $1;
+
+							$hash{$key} = $substr;} 	#строка
+						elsif 	($line =~ /$key":\s(-?\d+[\.eE]?\d*[-+]*\d*)?/) {
+							$hash{$key} = $1} 			#число
+						if 	($line =~ /$key":\s(\[.*?\])/) {
+							my $substr = $1;
+							
+							$hash{$key} = parse_json($substr);}
+						elsif   ($line =~ /$key":\s\{(.*?)\}/) {
+							my $substr = $1;
+							$hash{$key} = parse_json($substr);}	
+					}								
+				}
+
+	return \%hash;
+	}	
 }
-	return {%hash};
-}
+
 1;
+
